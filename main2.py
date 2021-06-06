3d57dbd085b971e0f8078ffddb5540f0a02e19f9
drimeF0 <67423319+drimeF0@users.noreply.github.com>
2021-06-06 20:07:22 +0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from json import dump,load
 from os import environ
 msg = ""
+ignore_id = []
 def save():
 	global msg
 	with open("save.cfg","w") as f:
@@ -11,13 +12,14 @@ def load():
 	global msg
 	with open("save.cfg") as f:
 		msg = load(msg,f)
-bot = commands.Bot(command_prefix="rass.")
+bot = commands.Bot(command_prefix="rass.",self_bot=True)
 
 #a
 @bot.event
 async def on_message(message):
 	global msg
 	global load
+	global ignore_id
 	try:
 		load()
 	except:
@@ -25,7 +27,9 @@ async def on_message(message):
 	if message.guild is None:
 		if not message.author.bot:
 			if msg:
-				await message.channel.send(msg)
+				if not message.author.id in ignore_id:
+					await message.channel.send(msg)
+					ignore_id.append(message.author.id)
 		await bot.process_commands(message)
 @commands.is_owner()
 @bot.command()
@@ -36,4 +40,4 @@ async def set_string(ctx,*,string):
 	await ctx.send("установлено")
 	save()
 
-bot.run(environ["TOKEN"])
+bot.run(environ["TOKEN"],bot=False)
