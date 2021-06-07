fbd7d1e0a0af19422ee4d6afffe0c1d1c487da16
drimeF0 <67423319+drimeF0@users.noreply.github.com>
2021-06-07 10:06:35 +0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -20,24 +20,24 @@ async def on_message(message):
 	global msg
 	global load
 	global ignore_id
+	await bot.process_commands(message)
 	try:
 		load()
 	except:
 		pass
 	if message.guild is None:
-		if not message.author.bot:
-			if msg:
-				if not message.author.id in ignore_id:
+		if msg:
+			if not message.author.id in ignore_id:
+				if message.author.id != bot.client.id:
 					await message.channel.send(msg)
 					ignore_id.append(message.author.id)
-		await bot.process_commands(message)
 @commands.is_owner()
 @bot.command()
 async def set_string(ctx,*,string):
 	global msg
 	global save
 	msg = string
-	await ctx.send("установлено")
+	await ctx.message.send("установлено")
 	save()
 
 bot.run(environ["TOKEN"],bot=False)
